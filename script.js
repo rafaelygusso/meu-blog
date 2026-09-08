f34d2de03d65b822657e60927931e227602c6cbb
rafaelygusso <rafaely.gusso@escola.pr.gov.br>
2026-09-08 09:03:08 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 
+const botoes = document.querySelectorAll("button");
+
+botoes.forEach(function (botao) {
+
+    let curtiu = false;
+
+    botao.addEventListener("click", botaoClicado);
+
+    function botaoClicado() {
+
+        let texto = botao.querySelector("span");
+
+        if (curtiu === false) {
+            texto.textContent++;
+            curtiu = true;
+        } else {
+            texto.textContent--;
+            curtiu = false;
+        }
+    }
+});
